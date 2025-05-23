aab51ff0ec02f3b273d4973b55202a5395357662
Jetze Zoethout <jzoethout@sogyo.nl>
2025-05-23 13:47:55 +0200

    Some ChatGPT code that works

---
+++ src/Metadata.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE InstanceSigs         #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Metadata where
+
+import           GHC.Generics
+import           GHC.TypeLits
+
+class GShowMeta f where
+  gshowMeta :: f p -> String
+
+instance (Datatype d) => GShowMeta (M1 D d f) where
+  gshowMeta :: Datatype d => M1 D d f p -> String
+  gshowMeta x = "This is datatype: " ++ datatypeName x
+
+instance (Constructor c) => GShowMeta (M1 C c f) where
+  gshowMeta :: Constructor c => M1 C c f p -> String
+  gshowMeta x = "Constructor: " ++ conName x
+
+class Selector s =>
+      KnownSelectorName s
+  where
+  getSelectorName :: M1 S s f p -> String
+
+type family GetSelName s :: Maybe Symbol where
+  GetSelName ('MetaSel name _ _ _) = name
+
+instance (Selector s, GetSelName s ~ 'Just name, KnownSymbol name) =>
+         KnownSelectorName s where
+  getSelectorName ::
+       (Selector s, GetSelName s ~ 'Just name, KnownSymbol name)
+    => M1 S s f p
+    -> String
+  getSelectorName x = "Selector: " ++ selName x
+
+instance (KnownSelectorName s) => GShowMeta (M1 S s f) where
+  gshowMeta :: KnownSelectorName s => M1 S s f p -> String
+  gshowMeta = getSelectorName
